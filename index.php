61e5b22907b3b1c5fd29d438fa003fc9032da8b7
Grace-Joy <33892212+Grace-Joy@users.noreply.github.com>
2018-06-18 20:24:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include('config.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/main.css"/>
+</head>
+<body>
+<div class="content">
+    <form action="" method="POST">
+        <ul style="list-style: none;">
+            <li>
+                <label>Username: </label>
+                <input class="simple-input" name="name" type="text" size="30"/>
+            </li>
+            <li>
+                <label>Names: </label>
+                <input class="simple-input" name="name" type="text" size="30"/>
+            </li>
+            <li>
+                <label>Email: </label>
+                <input class="simple-input" name="email" type="text" size="30"/>
+            </li>
+            <li>
+                <label>Password: </label>
+                <input class="simple-input" name="password" type="text" size="30"/>
+            </li>
+            <li>
+                <label>Confirm Password: </label>
+                <input class="simple-input" name="confirm_password" type="text" size="30"/>
+            </li>
+        </ul>
+        <input name="mySubmit" type="submit" value="Submit!"/>
+    </form>
+</div>
+</body>
+</html>
+
+</head>
+
+<body>
+
+</body>
---
+++ timeline.php
@@ -0,0 +1,54 @@
+<?php include('config.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/main.css"/>
+</head>
+<body>
+<div class="content">
+    <h3>TL</h3>
+    <div class="post-edit-layout">
+        <textarea placeholder="Hello Grace What's on your mind?"></textarea>
+        <button type="button">Post</button>
+    </div>
+    <hr>
+    <?php $sql = "SELECT id, text, time_created FROM posts";
+    if ($res = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($res) > 0) {
+            while ($row = mysqli_fetch_array($res)) {
+                ?>
+                <div class="post-layout">
+                    <span><?php echo $row['id']; ?></span><br>
+                    <div><?php echo $row['text']; ?></div>
+                    <p>comments count</p> <a href="#">like</a> <a href="#">dislike</a>
+                    <br>
+                    <p>time: <?php echo $row['time_created']; ?></p>
+                </div>
+                <div class="comment-layout">
+                    <span>username: text</span><br>
+                </div>
+                <div class="comments-edit-layout">
+                    <input class="simple-input" type="text" name="comment"/>
+                    <button type="button">comment</button>
+                </div>
+                <?php
+            }
+            $res->free();
+        } else {
+            echo "No matching records are found.";
+        }
+    } else {
+        echo "ERROR: Could not able to execute $sql. "
+            . mysqli_error($conn);
+    }
+    ?>
+
+</div>
+</body>
+</html>
+
+</head>
+
+<body>
+
+</body>
