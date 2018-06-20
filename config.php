1c6b847689fab85707751a92a256d47312e9f516
Grace-Joy <gracejoy.kanyanya@strathmore.edu>
2018-06-20 19:54:14 +0300

    SETUP OF XAMPP COMPLETE

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "localhost"; //can be "localhost" for local development
 $db_username = "root";
-$db_password = "pass";
+$db_password = "";
 $db_name = "facebook";
 $conn = new mysqli($db_host, $db_username, $db_password, $db_name) or die(mysqli_error($conn));
 ?>
